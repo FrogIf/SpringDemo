a9b77be18123085fae364ba3760f3ee7185f4462
frogif <skyfrog@163.com>
2020-05-04 15:26:46 +0800

    consul

--- CoffeeService/src/main/java/sch/frog/learn/spring/SpringAdminClientSecurityConfigure.java
+++ CoffeeService/src/main/java/sch/frog/learn/spring/SpringAdminClientSecurityConfigure.java
@@ -1,9 +1,15 @@
 package sch.frog.learn.spring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Configuration
 public class SpringAdminClientSecurityConfigure extends WebSecurityConfigurerAdapter {
@@ -17,6 +23,7 @@ public class SpringAdminClientSecurityConfigure extends WebSecurityConfigurerAda
         http.authorizeRequests()
                 .antMatchers("/assets/**").permitAll()
                 .antMatchers("/login").permitAll()
+                .antMatchers("/actuator/health").permitAll()    // 注意这个应该在下一个之前!!!
                 .antMatchers("/actuator/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
@@ -24,6 +31,21 @@ public class SpringAdminClientSecurityConfigure extends WebSecurityConfigurerAda
                 .logout().logoutUrl("/logout").and()
                 .httpBasic()
                 .and()
+                .addFilterBefore(new SecurityAccessLog(), ChannelProcessingFilter.class).httpBasic()    // 定义一个访问过滤器, 在所有认证之前
+                .and()
                 .csrf().disable();
+    }
+
+
+    @Slf4j
+    private static class SecurityAccessLog implements Filter{
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            if(servletRequest instanceof HttpServletRequest){
+                HttpServletRequest request = (HttpServletRequest) servletRequest;
+                log.info("access : {}", request.getRequestURL());
+            }
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
     }
 }

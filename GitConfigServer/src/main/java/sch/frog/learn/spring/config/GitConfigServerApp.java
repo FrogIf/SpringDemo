54d994ba25e5bf28c16e9d42b6e567994a2ff72a
frogif <skyfrog@163.com>
2020-05-16 08:39:52 +0800

    config server

---
+++ GitConfigServer/src/main/java/sch/frog/learn/spring/config/GitConfigServerApp.java
@@ -0,0 +1,15 @@
+package sch.frog.learn.spring.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class GitConfigServerApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(GitConfigServerApp.class, args);
+    }
+
+}
